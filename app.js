33317ff0a451a85e7833db26da6686554317f260
Yi-Hsiu Lee <tmrmds.yhl@gmail.com>
2023-05-28 16:37:51 +0800

    add routers

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const routes = require('./routes') // 引用路由器
+const methodOverride = require('method-override')
+
+require('./config/mongoose')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.static('public'))
-
-
-// 設定首頁路由
-app.get('/', (req, res) => {
-    res.render('index')
-})
+app.use(methodOverride('_method'))
+app.use(routes) // 將 request 導入路由器
 
 // 設定 port 3000
 app.listen(3000, () => {
