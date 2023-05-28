34f8916c0f82f06db9792f9e8079cefd5ca0ded6
Yi-Hsiu Lee <tmrmds.yhl@gmail.com>
2023-05-28 19:54:00 +0800

    add feat: login validation

--- routes/modules/login.js
+++ routes/modules/login.js
@@ -5,8 +5,27 @@ const Account = require('../../models/Account') // 引用 Todo model
 
 // 定義首頁路由
 // index
-router.get('/', (req, res) => {
-    res.render('index')
+router.post('/', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    (async () => {
+        try {
+            // Check if there is the same link
+            const user = await Account.findOne({ email }).lean()
+            if (user) {
+                if (user.password === password) {
+                    res.send(`Welcome back, ${user.firstName}`)
+                } else {
+                    res.send(`Incorrect password for user ${email}`)
+                }
+            } else {
+                res.send(`These is no user ${email}`)
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    })();
 })
 
 // 匯出路由模組
